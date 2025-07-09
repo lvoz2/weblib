29618de6118f5b40bfe12b020cc1839cb16cb6c3
lvoz2 <lv_oz2@icloud.com>
2025-07-10 09:58:06 +1000

    Main page styling mostly done

--- app.py
+++ app.py
@@ -5,12 +5,35 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 @app.get("/")
 @app.get("/index.html")
-def index():
-    """index.html for site
-    """
-    return flask.render_template("index.html")
+def index() -> str:
+    """index.html for site"""
+    return flask.render_template(
+        "index.html",
+        saved_items=[],
+        recent_items=[],
+    )
+
+
+@app.get("/browse")
+def browse() -> str:
+    """browse page for site"""
+    return flask.render_template("browse.html")
+
+
+@app.get("/query")
+def query() -> str:
+    """ask question page for site"""
+    return flask.render_template("query.html")
+
+
+@app.get("/saved")
+def saved() -> str:
+    """saved items page for site"""
+    return flask.render_template("saved.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
